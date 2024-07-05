5618d0fb943c1ab50a55adba1c7160f6c385973e
Lepionnier92 <134221586+Lepionnier92@users.noreply.github.com>
2024-07-05 19:19:04 +0000

    #

--- config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const apiUrl = process.env.API_URL;
-export const apiKey = process.env.API_KEY;
-export const client_id = process.env.CLIENT_ID;
